058e4cabb8b33419d46c48878e8a9afcae1d5eb7
sherouk22 <sheroukmohy558@gmail.com>
2025-02-23 11:29:45 +0200

    Add project files.

---
+++ Assignment03_Linq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment03_Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
